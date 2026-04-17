b1f52c6d88eb9b7a520d30625e0d3a2853503e45
Viet Nguyen <vietnguyen8125@gmail.com>
2026-04-17 13:12:32 +0700

    final ver
    combine everyone's works into final sln

---
+++ QLNK/Program.cs
@@ -0,0 +1,25 @@
+﻿using QLNK;
+using System;
+using System.Windows.Forms;
+
+namespace QuanLyNhanKhau_Nhom6
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+
+    /// <summary>
+    /// Lưu thông tin phiên đăng nhập hiện tại (dùng cho CONTEXT_INFO của SQL).
+    /// </summary>
+    public static class UserSession
+    {
+        public static int CurrentUserId = -1;
+    }
+}
